494272c44d19c69ea45f8dfe2ff87ed8e497a1a5
Souvik Chakraborty <140975708+ChakraborttySouvik@users.noreply.github.com>
2024-08-24 12:12:52 +0530

    Add files via upload

---
+++ DjangoChat/DjangoChat/asgi.py
@@ -0,0 +1,18 @@
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from ChitChat import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChat.settings')
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        ),
+    }
+)
